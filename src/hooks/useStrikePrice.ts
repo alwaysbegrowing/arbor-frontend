db882bfd5e59e982dfd3f5fe987e16002484ba50
Kyle <kyletrusler@gmail.com>
2022-09-15 18:54:06 +0100

    remove token price from strike price

--- src/hooks/useStrikePrice.ts
+++ src/hooks/useStrikePrice.ts
@@ -1,8 +1,6 @@
 import { useFormContext } from 'react-hook-form'
 import { useToken } from 'wagmi'
 
-import { useTokenPrice } from './useTokenPrice'
-
 export const useStrikePrice = () => {
   const { watch } = useFormContext()
 
@@ -12,10 +10,12 @@ export const useStrikePrice = () => {
     'borrowToken',
     'collateralToken',
   ])
-  const { data: collateralTokenPrice } = useTokenPrice(collateralToken?.address)
   const { data: collateralTokenData } = useToken({ address: collateralToken?.address })
   const { data: borrowTokenData } = useToken({ address: borrowToken?.address })
-  const value = 1 / ((amountOfConvertible / amountOfBonds) * collateralTokenPrice)
+
+  // Strike price: Price at which the value of the convertible tokens equals the amount owed at maturity
+  // Strike Price (USDC/Collateral) = Bonds (USDC) / amountOfConvertible (Collateral)
+  const value = amountOfBonds / amountOfConvertible
 
   const display = `${(value || 0).toLocaleString()} ${borrowTokenData?.symbol}/${
     collateralTokenData?.symbol
