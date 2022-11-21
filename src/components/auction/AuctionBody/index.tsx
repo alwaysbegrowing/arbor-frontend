7bdc3344fbb7045233ae740660267b32bc2be9cb
Kyle <kyletrusler@gmail.com>
2022-11-21 08:53:07 -0800

    arbor.garden -> arbor.finance

--- src/components/auction/AuctionBody/index.tsx
+++ src/components/auction/AuctionBody/index.tsx
@@ -64,7 +64,7 @@ const WarningCard = () => {
             decisions.{' '}
             <a
               className="text-sm font-normal normal-case underline"
-              href="https://docs.arbor.garden"
+              href="https://docs.arbor.finance"
               rel="noreferrer"
               target="_blank"
             >
