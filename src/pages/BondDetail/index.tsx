5f8f99cb29450059d8cb56220494e040b6853b1b
Bookcliff <bookcliff76@gmail.com>
2022-12-05 14:03:10 -0600

    fix maturity error

--- src/pages/BondDetail/index.tsx
+++ src/pages/BondDetail/index.tsx
@@ -118,7 +118,10 @@ export const getBondStates = (
   const isPartiallyPaid = bond?.maxSupply - bond?.amountUnpaid > 0 && isDefaulted
   const isPaid = bond?.state === 'paidEarly' || bond?.state === 'paid'
   const isActive = bond?.state === 'active'
-  const isMatured = isDefaulted || bond?.state === 'paid'
+  const isMatured =
+    isDefaulted ||
+    bond?.state === 'paid' ||
+    bond?.maturityDate < dayjs(new Date()).unix().toString()
   return {
     isMatured,
     isConvertBond,
